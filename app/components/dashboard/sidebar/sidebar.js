a58e914b0c2759dcd2c33173e6cb590d96b799f1
IntelligenceHubCreates <intelligencehub20@gmail.com>
2024-01-13 15:58:50 +0530

    commit1

---
+++ app/components/dashboard/sidebar/sidebar.js
@@ -0,0 +1,107 @@
+import Image from "next/image";
+import MenuLink from "./menulink/menulink";
+import styles from "./sidebar.module.css"
+import {
+    MdDashboard,
+    MdAttachMoney,
+    MdShoppingBag,
+    MdSupervisedUserCircle,
+    MdWork,
+    MdAnalytics,
+    MdPeople,
+    MdOutlineSettings,
+    MdHelpCenter,
+    MdLogout
+} from "react-icons/md";
+
+const menuItems = [
+    {
+        title: "pages",
+        list: [
+            {
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: <MdDashboard />
+            },
+            {
+                title: "Users",
+                path: "/dashboard/users",
+                icon: <MdSupervisedUserCircle />
+            },
+            {
+                title: "Products",
+                path: "/dashboard/products",
+                icon: <MdShoppingBag />
+            },
+            {
+                title: "Transactions",
+                path: "/dashboard/transactions",
+                icon: <MdAttachMoney />
+            },
+        ],
+    },
+    {
+        title: "Analytics",
+        list:[
+            {
+                title: "Revenue",
+                path: "/dashboard/revenue",
+                icon: <MdWork />
+            },
+            {
+                title: "Reports",
+                path: "/dashboard/report",
+                icon: <MdAnalytics />
+            },
+            {
+                title: "Teams",
+                path: "/dashboard/teams",
+                icon: <MdPeople />
+            },
+        ],
+    },
+    {
+        title: "Users",
+        list: [
+            {
+                title: "Settings",
+                path: "/dashboard/settings",
+                icon: <MdOutlineSettings />
+            },
+            {
+                title: "Help",
+                path: "/dashboard/help",
+                icon: <MdHelpCenter />
+            },
+        ],
+    },
+];
+const SideBar = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.user}>
+                <Image className={styles.userImage} src="" alt="" width="50" height="50"/>
+                <div className={styles.userDetails}>
+                    <span className={styles.username}>Varsha Jain</span>
+                    <span className={styles.userTitle}>Administrator</span>
+                </div>
+            </div>
+            <ul className={styles.list}>
+                {menuItems.map( (cat) => (
+                    <li key={cat.title}>
+                        <span className={styles.cat}>{cat.title}</span>
+                        {cat.list.map(item=>(
+                            <MenuLink item={item} key={item.title}/>
+                        ))}
+                    </li>
+                ))}
+            </ul>
+            <button className={styles.logout}>
+                <MdLogout/>
+                Logout
+            </button>
+        </div>
+    )
+}
+
+export default SideBar
